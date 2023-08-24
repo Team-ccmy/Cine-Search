8351f16f54a2bd534be3851d10344625357ff714
Yega Cho <yega1009@gmail.com>
2023-08-24 18:04:09 -0400

    defined dom elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const form = document.querySelector('.searchForm');
+    const input = form.querySelector('input');
+    const searchResultCard = document.querySelector('.searchResultCard');
+    
+})

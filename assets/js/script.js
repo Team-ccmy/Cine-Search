608ee92624d6670e47bbd7a2844f28669110e764
Carlos Macias <cimm21187@gmail.com>
2023-08-25 13:25:49 -0400

    Implemented filtering.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,30 @@
 const apiKey = 'ff2971a496e122549ee3b82e1c22d1e9';
 
 document.addEventListener('DOMContentLoaded', function () {
-
-    var searchForm = document.querySelector('.searchForm');
-    var queryInput = searchForm.querySelector('input');
-    var searchResults = document.querySelector('.search-result');
-    var searchResultCard = document.querySelector('.searchResultCard');
+    const searchForm = document.getElementById('searchForm');
+    const searchInput = document.getElementById('search-input');
 
     // Load movie genres from the API and populate the genre dropdown
     function loadGenres() {
-        var apiKey = 'ff2971a496e122549ee3b82e1c22d1e9';
-        var apiUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + apiKey + "&language=en-US";
-
+        const apiKey = 'ff2971a496e122549ee3b82e1c22d1e9';
+        const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
 
         // Use the fetch function to get data from the API
         fetch(apiUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                var genres = data.genres;
-                console.log(genres);
-                var genreFilter = document.getElementById('genre-filter');
+            .then(response => response.json())
+            .then(data => {
+                const genres = data.genres;
+                const genreFilter = document.getElementById('genre-filter');
 
                 genres.forEach(function (genre) {
-                    var option = document.createElement('option');
+                    let option = document.createElement('option');
                     option.value = genre.id;
                     option.textContent = genre.name;
                     genreFilter.appendChild(option);
                 })
 
-                var selectItems = document.querySelectorAll('select');
-                M.FormSelect.init(selectItems);
+                const selectItems = document.querySelectorAll('select');
+                const selectInstances = M.FormSelect.init(selectItems);
             })
             .catch(function (error) {
                 console.error('Error fetching genres:', error);
@@ -72,35 +65,9 @@ document.addEventListener('DOMContentLoaded', function () {
         const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
         fetch(apiUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                var movies = data.results;
-                console.log(movies);
-                searchResultCard.innerHTML = '';
-
-                movies.forEach(function (movie) {
-                    var title = movie.title, release_date = movie.release_date, poster_path = movie.poster_path;
-                    var result = document.createElement('div');
-                    result.classList.add('result');
-                    result.innerHTML =
-                        '<div class="card">' +
-                        '<div class="card-image waves-effect waves-block waves-light">' +
-                        '<img class="activator" src="https://image.tmdb.org/t/p/w185/' + poster_path + '" alt="' + title + ' poster">' +
-                        '</div>' +
-                        '<div class="card-content">' +
-                        '<span class="card-title activator grey-text text-darken-4 movieTitle">' + title + '</span>' +
-                        '<i class="material-icons center-align"><a href="#">delete_sweep</a></i>' +
-                        '<i class="material-icons center-align"><a href="#">queue_play_next</a></i>' +
-                        '</div>' +
-                        '<div class="card-reveal">' +
-                        '<span class="card-title grey-text text-darken-4">' + title + '<i class="material-icons right">close</i></span>' +
-                        '<p>Here is some more information about this movie.</p>' +
-                        '</div>' +
-                        '</div>';
-                    searchResultCard.appendChild(result);
-                });
+            .then(response => response.json())
+            .then(data => {
+                displayResults(data.results);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
@@ -140,27 +107,29 @@ document.addEventListener('DOMContentLoaded', function () {
     // Event listener for the form submission with both query and filters
     searchForm.addEventListener('submit', function (event) {
         event.preventDefault();
-        var query = queryInput.value.trim();
-        var apiKey = 'ff2971a496e122549ee3b82e1c22d1e9';
+        const query = searchInput.value.trim();
+        const apiKey = 'ff2971a496e122549ee3b82e1c22d1e9';
 
         // Check if a search query is provided or not
-        var apiUrl;
+        let apiUrl;
         if (query) {
-            apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&query=' + query;
+            apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
         } else {
-            apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey;
+            apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
         }
 
         // Fetch by applied filters
-        var genreFilter = document.getElementById('genre-filter').value;
-        var ratingFilter = document.getElementById('rating-filter').value;
-        var yearFilter = document.getElementById('year-filter').value;
-        var languageFilter = document.getElementById('language-filter').value;
+        const genreFilter = document.getElementById('genre-filter').value;
+        const alphabeticalOrder = document.getElementById('alphabetical-filter').value;
+        const ratingFilter = document.getElementById('rating-filter').value;
+        const yearFilter = document.getElementById('year-filter').value;
+        const languageFilter = document.getElementById('language-filter').value;
 
-        if (genreFilter) apiUrl += '&with_genres=' + genreFilter;
-        if (ratingFilter) apiUrl += '&vote_average.gte=' + ratingFilter;
-        if (yearFilter) apiUrl += '&primary_release_year=' + yearFilter;
-        if (languageFilter) apiUrl += '&with_original_language=' + languageFilter;
+        if (genreFilter) apiUrl += `&with_genres=${genreFilter}`;
+        if (alphabeticalOrder) apiUrl += `&sort_by=original_title.${alphabeticalOrder}`;
+        if (ratingFilter) apiUrl += `&vote_average.gte=${ratingFilter}`;
+        if (yearFilter) apiUrl += `&primary_release_year=${yearFilter}`;
+        if (languageFilter) apiUrl += `&with_original_language=${languageFilter}`;
 
         fetchMovies(apiUrl);
     });
@@ -175,63 +144,36 @@ document.addEventListener('DOMContentLoaded', function () {
         saveToLocalStorage('bucket', movieId, movieTitle);
     }
 
-    loadMoviesFromLocalStorage('.bucket ul');
-    loadMoviesFromLocalStorage('.queue ul');
-
-    // Function to add movie to a specific list
-    function addMovieToList(movieTitle, listSelector) {
-        var list = document.querySelector(listSelector);
-
-        // Check for duplicates
-        var listItemExists = Array.from(list.children).some(function (li) {
-            return li.textContent === movieTitle;
-        });
-
-        if (listItemExists) {
-            console.log('Movie: ' + movieTitle + ' already exists in the list.');
-            return;
-        }
-
-        var listItem = document.createElement('li');
+    function addToQueue(movieId, movieTitle) {
+        const queueList = document.querySelector('.queue ul');
+        const listItem = document.createElement('li');
+        listItem.dataset.id = movieId;
         listItem.textContent = movieTitle;
         queueList.appendChild(listItem);
 
-        var storageKey = listSelector.replace(' ', '').replace('.', '');
-        var currentList = JSON.parse(localStorage.getItem(storageKey) || '[]');
-        currentList.push(movieTitle);
-        localStorage.setItem(storageKey, JSON.stringify(currentList));
+        saveToLocalStorage('queue', movieId, movieTitle);
     }
 
-    // Function to handle the click event on the movie icons
-    function handleIconClick(event) {
-        var target = event.target;
-
-        console.log("Clicked on:", target.textContent);
-
-        if (target.tagName !== 'A') {
-            target = target.parentElement;
-        }
+    function saveToLocalStorage(listName, movieId, movieTitle) {
+        const existingList = JSON.parse(localStorage.getItem(listName)) || [];
+        existingList.push({ id: movieId, title: movieTitle });
+        localStorage.setItem(listName, JSON.stringify(existingList));
+    }
 
     function loadFromLocalStorage() {
         const bucketList = document.querySelector('.bucket ul');
         const queueList = document.querySelector('.queue ul');
 
-        var card = target.closest('.card');
-        console.log("Parent card:", card);
-        if (!card) return;
+        // Clear the current list in the DOM
+        bucketList.innerHTML = '';
+        queueList.innerHTML = '';
 
-        var titleElement = card.querySelector('.movieTitle');
-        console.log("Title element:", titleElement);
-        if (!titleElement) return;
+        const bucketListData = JSON.parse(localStorage.getItem('bucket')) || [];
+        bucketListData.forEach(movie => addToBucket(movie.id, movie.title));
 
-        var movieTitle = titleElement.textContent;
-        console.log("Movie title:", movieTitle);
-
-        if (target.textContent === 'delete_sweep') {
-            addMovieToList(movieTitle, '.bucket ul');
-        } else if (target.textContent === 'queue_play_next') {
-            addMovieToList(movieTitle, '.queue ul');
-        }
+        const queueListData = JSON.parse(localStorage.getItem('queue')) || [];
+        queueListData.forEach(movie => addToQueue(movie.id, movie.title));
+    }
 
     function clearBucket() {
         const bucketList = document.querySelector('.bucket ul');
